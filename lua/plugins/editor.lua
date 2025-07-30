841760fd5779f898ca1d54821bc367504b73f148
rizkirakasiwi <rizkirakasiwi09@gmail.com>
2025-07-30 12:11:23 +0700

    update telescope bind into <C-f>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -73,7 +73,7 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         cmd = "Telescope",
         keys = {
-            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Fuzzy find files in cwd" },
+            { "<C-f>", "<cmd>Telescope find_files<cr>", desc = "Fuzzy find files in cwd" },
             { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Fuzzy find recent files" },
             { "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Find string in cwd" },
             { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor in cwd" },
