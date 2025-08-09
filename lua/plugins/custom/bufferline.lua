5710562d5c85ce1bc21d67619f3d25f7a4262211
rizkirakasiwi <rizkirakasiwi09@gmail.com>
2025-08-09 20:43:04 +0700

    adjust bufferline

--- lua/plugins/custom/bufferline.lua
+++ lua/plugins/custom/bufferline.lua
@@ -40,10 +40,10 @@ return {
                     max_prefix_length = 30,
                     truncate_names = true,
                     tab_size = 21,
-                    diagnostics = "nvim_lsp",
+                    diagnostics = "nvim_lsp" | "coc",
                     diagnostics_update_in_insert = false,
                     diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                        local icon = level:match("error") and " " or " "
+                        local icon = level:match("error") and " " or " "
                         return " " .. icon .. count
                     end,
                     custom_filter = function(buf_number, buf_numbers)
