a33c9d8b7591db39a8e4e2ec0d25e027096dcf03
rizkirakasiwi <rizkirakasiwi09@gmail.com>
2025-10-04 17:00:08 +0700

    change color scheme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,12 +1,31 @@
 return {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {
-        style = "moon",
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        styles = {
+            comments = { "italic" },
+            conditionals = { "italic" },
+        },
+        float = {
+            transparent = true,
+        },
+        custom_highlights = function(colors)
+            return {
+                LineNr = { fg = colors.overlay2 },
+                CursorLineNr = { fg = colors.lavender, style = { "bold" } },
+                TabLineSel = { bg = colors.pink },
+                CmpBorder = { fg = colors.surface2 },
+                Pmenu = { bg = colors.none },
+                Visual = { bg = colors.pink, fg = colors.base, style = { "bold" } },
+            }
+        end,
     },
     config = function(_, opts)
-        require("tokyonight").setup(opts)
-        vim.cmd.colorscheme("tokyonight")
+        require("catppuccin").setup(opts)
+        vim.cmd.colorscheme("catppuccin")
     end,
 }
